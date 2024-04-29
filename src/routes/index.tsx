403ce82531028e7ce1818fc4bca847b6c89a3ee2
shehlaMushtaqTkxel <156066478+shehlaMushtaqTkxel@users.noreply.github.com>
2024-04-29 14:02:10 +0500

    fix in AppRout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AdminSignIn from "@/views/admin/signIn";
 import AdminRoutes from "./adminRoutes";
 import  {TestComponent}  from "../components/TestComponent";
@@ -9,7 +9,8 @@ const AppRoutes = () => {
       <Route path="/sign-in" element={<AdminSignIn />} />
       <Route path="/admin/*" element={<AdminRoutes />} />
       <Route path="/test-component" element={<TestComponent/>} />
-      <Route path="/" element={<>check</>} />
+      <Route path="/" element={<Navigate to="/sign-in" />} />
+
     </Routes>
   );
 };
