fb8de45f7b7ba4556d8963b7a90457b590b96f83
Mamoon Tariq <fullstack4.dev@globaldrum.social>
2024-04-29 16:41:45 +0500

    dev branch setup

--- src/helpers/validations/AdminForms.ts
+++ src/helpers/validations/AdminForms.ts
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 export const signInValidationSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Email is required"),
+  email: Yup.string().email("invalid email").required("Email is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
