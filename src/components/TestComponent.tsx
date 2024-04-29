5b82a1cd1ebf1bd6a1bb016efd038375bbe58aae
shehlaMushtaqTkxel <156066478+shehlaMushtaqTkxel@users.noreply.github.com>
2024-04-29 11:08:24 +0500

    styles and login form

--- src/components/TestComponent.tsx
+++
@@ -1,6 +0,0 @@
-import { Typography } from "@mui/material"
-
- export const TestComponent=()=>{
-    return<Typography>test</Typography>
-    
- }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AdminSignIn from "@/views/admin/signIn";
 import AdminRoutes from "./adminRoutes";
-import  {TestComponent}  from "../components/TestComponent";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/sign-in" element={<AdminSignIn />} />
       <Route path="/admin/*" element={<AdminRoutes />} />
-      <Route path="/test-component" element={<TestComponent/>} />
       <Route path="/" element={<>check</>} />
     </Routes>
   );
