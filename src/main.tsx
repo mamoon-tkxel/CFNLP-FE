48f5119d75796e9817218069d03e9afbb981a6ea
shehlaMushtaqTkxel <156066478+shehlaMushtaqTkxel@users.noreply.github.com>
2024-04-29 14:14:28 +0500

    removed extra line from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ Promise.all([import("@/Root"), import("@/App")]).then(
     render(App);
   }
 );
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
 import './styles/base/normalize.scss'
 import './styles/base/_fonts.scss'
 import './styles/base/_typography.scss'
 // import './styles/base/globalStyles.scss'
 import './styles/base/input.scss'
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
